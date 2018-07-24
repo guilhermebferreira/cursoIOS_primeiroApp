85446f386ebf95021666bb321859525e634d7e46
labmacmini12 <labmacmini12@mac-mini-de-labmacmini12.local>
2018-07-24 09:17:46 -0300

    projeto inicial para iOS com Swift

--- MeuPrimeiroApp/AppDelegate.swift
+++ MeuPrimeiroApp/AppDelegate.swift
@@ -6,6 +6,11 @@
 //  Copyright © 2018 cursoIOS. All rights reserved.
 //
 
+/*
+ * serve para controlar os estados da aplicação
+ * (não tem nada haver com telas)
+ */
+
 import UIKit
 
 @UIApplicationMain
@@ -15,30 +20,42 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        print("APLICAÇÃO TERMINOU O CARREGAMENTO")
         return true
     }
 
+    //função chamada quando o app esta prestes a perder o foco
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        print("VAI FICAR INATIVO")
     }
 
+    //função chamada quando o app já perdeu o foco
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        print("APLICACAO PERDEU O FOCO")
+        
     }
 
+    //função chamada quando o app esta prestes a ganhar o foco
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        print("APLICACAO IRA SE TORNAR ATIVA")
+        
     }
 
+    //função chamada quando o app já recebeu o foco
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        print("O APP JÁ SE TORNOU ATIVO")
     }
 
+    //função chamada quando o app esta prestes a ser destruído
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        print("ESTA APLICAÇÃO SERÁ FINALIZADA")
     }
 
 
--- MeuPrimeiroApp/ViewControllerTela2.swift
+++ MeuPrimeiroApp/ViewControllerTela2.swift
@@ -1,55 +1,60 @@
 //
-//  ViewController.swift
+//  ViewControllerTela2.swift
 //  MeuPrimeiroApp
 //
-//  Created by labmacmini12 on 23/07/2018.
+//  Created by labmacmini12 on 24/07/2018.
 //  Copyright © 2018 cursoIOS. All rights reserved.
 //
 
-/*
- *controla os estados de uma tela/view
- */
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewControllerTela2: UIViewController {
 
     //CHAMADO QUANDO UMA TELA TERMINOU DE SER CARREGADA
     override func viewDidLoad() {
         // Do any additional setup after loading the view, typically from a nib.
-        print("TELA 1 TERMINOU O CARREGAMENTO")
+        print("TELA 2 TERMINOU O CARREGAMENTO")
         super.viewDidLoad()
     }
     
     //CHAMADO QUANDO UMA TELA ESTA PRESTES A SER EXIBIDA, MAS AINDA NÃO FOI
     override func viewWillAppear(_ animated: Bool) {
-        print("TELA 1 ESTA PRESTES A SER EXIBIDA")
+        print("TELA 2 ESTA PRESTES A SER EXIBIDA")
     }
     
     //CHAMADO QUANDO A TELA JÁ ESTA SENDO EXIBIDA
     override func viewDidAppear(_ animated: Bool) {
-        print("TELA 1 ESTA SENDO EXIBIDA")
+        print("TELA 2 ESTA SENDO EXIBIDA")
     }
     
     //CHAMADO QUANDO A TELA ESTA PRESTES A SAIR DO FOREGROUND
     override func viewWillDisappear(_ animated: Bool) {
-        print("A TELA 1 IRA PERDER O FOCO")
+        print("A TELA 2 IRA PERDER O FOCO")
     }
     
     //CHAMADO QUANDO A TELA JÁ PERDEU O FOCO
     override func viewDidDisappear(_ animated: Bool) {
-        print("TELA 1 PERDEU O FOCO")
+        print("TELA 2 PERDEU O FOCO")
     }
-
+    
     //AVISO SOBRE BAIXA DISPONIBILIDADE DE MEMORIA
     override func didReceiveMemoryWarning() {
         // Dispose of any resources that can be recreated.
         //O PROXIMO PASSO APÓS RECEBER ESSE AVISO, PODE SER O SO MATANDO O SEU APLICATIVO
         print("CUIDADO! POUCA MEMORIA")
         super.didReceiveMemoryWarning()
-
+        
     }
 
 
-}
+    /*
+    // MARK: - Navigation
 
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
